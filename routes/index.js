fcda986a49b5f11554e44f3e82428503f27a13c9
Jeffrey Klaus <jpk1215@gmail.com>
2014-08-20 20:25:46 -0400

    end of first day

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+
 router.get('/', function(req, res) {
   models.Hotel.find(function(err, results) {
-    res.render('index', { hotels: results, title: "Trip Planner" });
+    res.render('index', { hotels: models["Hotel"], title: "Trip Planner" });
   });
 });
 
